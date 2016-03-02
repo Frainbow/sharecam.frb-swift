6001d94dda37ca44fa118b8a55029e46c04b3acd
郭宗麟 <tlkuo.frb@gmail.com>
2016-03-02 10:23:10 +0800

    Initial swift project

---
+++ ShareCam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareCam
+//
+//  Created by martin on 2016/3/2.
+//  Copyright © 2016年 Flying Rainbow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
